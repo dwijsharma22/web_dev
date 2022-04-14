da779786954e74399d0f8a9d8135c76eb5f74cf1
dwijsharma22 <dwijsharma22@gmail.com>
2022-04-14 10:01:17 +0530

    Final Touch

--- main.js
+++ main.js
@@ -2,25 +2,25 @@ let answer = document.getElementById("answer");
 
 inputs = document.querySelectorAll(".buttons input");
 
-let screenvalue = '';
+
 
 inputs.forEach(inp => {
     inp.addEventListener("click", (e) => {
         text = e.target.value;
 
         if(text == "AC"){
-            screenvalue = '';
-            answer.value = screenvalue;
+            answer.value = '';
+            answer.value = answer.value;
         }
         else if(text == "="){
-            answer.value = eval(screenvalue);
+            answer.value = eval(answer.value);
         }
         else if(text == "DEL"){
             answer.value = answer.value.substr(0, answer.value.length - 1);
         }
         else{
-            screenvalue += text;
-            answer.value = screenvalue;
+            answer.value += text;
+            answer.value = answer.value;
             
         }
     });
